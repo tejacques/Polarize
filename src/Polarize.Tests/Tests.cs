bcbaa39e5f90abf795ad34733d7b8fc1c92dced8
Tom Jacques <tejacques@gmail.com>
2014-02-09 23:54:31 -0500

    Working but slow version

---
+++ src/Polarize.Tests/Tests.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polarize.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        Dictionary<string, Dictionary<string, int>> testDictionary;
+        int loops = 100000;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            testDictionary = new Dictionary<string, Dictionary<string, int>>
+            {
+                { "A", new Dictionary<string, int>
+                    {
+                        {"a", 1},
+                        {"b", 2},
+                        {"c", 3}
+                    }
+                },
+                { "B", new Dictionary<string, int>
+                    {
+                        {"i", 1},
+                        {"j", 2},
+                        {"k", 3}
+                    }
+                },
+                { "C", new Dictionary<string, int>
+                    {
+                        {"x", 1},
+                        {"y", 2},
+                        {"z", 3}
+                    }
+                }
+            };
+        }
+
+        [Test]
+        public void A_Jit()
+        {
+            var tempLoops = loops;
+            loops = 1;
+            TestFilterSpeed();
+            TestFilterFieldsSpeed();
+            loops = tempLoops;
+        }
+
+        [Test]
+        public void TestFilter()
+        {
+            JsonFilter<Dictionary<string, Dictionary<string, int>>> filter = testDictionary;
+
+            var json = JsonConvert.SerializeObject(filter);
+            var expected = "{\"A\":{\"a\":1,\"b\":2,\"c\":3},\"B\":{\"i\":1,\"j\":2,\"k\":3},\"C\":{\"x\":1,\"y\":2,\"z\":3}}";
+
+            Assert.AreEqual(expected, json);
+        }
+
+        [Test]
+        public void TestFilterSpeed()
+        {
+            JsonFilter<Dictionary<string, Dictionary<string, int>>> filter = testDictionary;
+            for(int i = 0; i < loops; i++)
+            {
+                var json = JsonConvert.SerializeObject(filter);
+            }
+        }
+
+        [Test]
+        public void TestFilterFields()
+        {
+            JsonFilter<Dictionary<string, Dictionary<string, int>>> filter =
+                JsonFilter.Create(testDictionary, "A", "B.i", "C.z");
+
+            var json = JsonConvert.SerializeObject(filter);
+            var expected = "{\"A\":{\"a\":1,\"b\":2,\"c\":3},\"B\":{\"i\":1},\"C\":{\"z\":3}}";
+            Assert.AreEqual(expected, json);
+        }
+
+        [Test]
+        public void TestFilterFieldsSpeed()
+        {
+            JsonFilter<Dictionary<string, Dictionary<string, int>>> filter =
+                JsonFilter.Create(testDictionary, "A", "B.i", "C.z");
+            for (int i = 0; i < loops; i++)
+            {
+                var json = JsonConvert.SerializeObject(filter);
+            }
+        }
+    }
+}
---
+++ src/Polarize/JsonFilter.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polarize
+{
+    [JsonConverter(typeof(JsonFilterSerializer))]
+    public class JsonFilter
+    {
+        public object Value;
+        public readonly string[] Fields;
+        public readonly HashSet<string> FieldPrefixSet;
+        public readonly HashSet<string> FieldSet;
+
+        public static JsonFilter<T> Create<T>(T value, params string[] fields)
+        {
+            return new JsonFilter<T>(value, fields);
+        }
+
+        internal JsonFilter(object value, string[] fields)
+        {
+            Value = value;
+
+            Array.Sort(fields);
+            Fields = fields;
+            FieldSet = new HashSet<string>(fields);
+            FieldPrefixSet = new HashSet<string>(
+                fields.SelectMany(field =>
+                {
+                    var splitFields = field.Split(StringSplits.Period);
+                    string[] fieldPaths = new string[splitFields.Length];
+                    StringBuilder sb = new StringBuilder(splitFields[0]);
+                    fieldPaths[0] = sb.ToString();
+                    for (int i = 1; i < splitFields.Length; i++)
+                    {
+                        sb.Append('.');
+                        sb.Append(splitFields[i]);
+                        fieldPaths[i] = sb.ToString();
+                    }
+
+                    return fieldPaths;
+                }));
+        }
+    }
+
+    [JsonConverter(typeof(JsonFilterSerializer))]
+    public class JsonFilter<T> : JsonFilter
+    {
+        internal JsonFilter(T value, string[] fields)
+            : base(value, fields) {}
+
+        public static implicit operator JsonFilter<T>(T t)
+        {
+            return JsonFilter.Create(t);
+        }
+    }
+}
